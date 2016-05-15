3ac5cef547c1839faa09ae6beb4ef21734946f35
J B <eugeni777@yandex.ru>
2016-05-15 16:47:40 +0300

    First commit of RPS game

---
+++ src/main/java/com/lottoland/GameLauncher.java
@@ -0,0 +1,13 @@
+package com.lottoland;
+
+/**
+ * Reads console, launches game
+ *
+ */
+public class GameLauncher {
+
+	public static void main(String[] args) {
+		System.out.println("Launching the game!");
+	}
+
+}
---
+++ src/test/java/com/lottoland/GameLauncherTest.java
@@ -0,0 +1,12 @@
+package com.lottoland;
+
+import org.junit.Test;
+
+public class GameLauncherTest {
+
+	@Test
+	public void mainTest() {
+		GameLauncher.main(new String[] {"test"});
+	}
+
+}
