1da94beec8c2028e70dcdc721214050014dda478
J B <eugeni777@yandex.ru>
2016-05-15 18:25:34 +0300

    Meaningfull output for RandomPlayerTest

--- src/test/java/com/lottoland/game/player/RandomPlayerTest.java
+++ src/test/java/com/lottoland/game/player/RandomPlayerTest.java
@@ -25,36 +25,54 @@ public class RandomPlayerTest {
 	
 	@Test
 	public void testNextMoveIsRock() {
+		System.out.printf("\n\n***Testing out for '%s' move***\n", MoveType.ROCK);
+		
 		boolean isLoop = true;
+		int count = 0;
 		
 		while (isLoop) {
+			count++;
 			MoveType nextMove = randomPlayer.nextMove();
+			System.out.printf("Next move is '%s'\n", nextMove.toString());
 			if (nextMove.equals(MoveType.ROCK)) {
 				isLoop = false;
+				System.out.printf("Got '%s' move after %d iterations\n", MoveType.ROCK, count);
 			}
 		}
 	}
 	
 	@Test
 	public void testNextMoveIsPaper() {
+		System.out.printf("\n\n***Testing out for '%s' move***\n", MoveType.PAPER);
+		
 		boolean isLoop = true;
+		int count = 0;
 		
 		while (isLoop) {
+			count++;
 			MoveType nextMove = randomPlayer.nextMove();
+			System.out.printf("Next move is '%s'\n", nextMove.toString());
 			if (nextMove.equals(MoveType.PAPER)) {
 				isLoop = false;
+				System.out.printf("Got '%s' move after %d iterations\n", MoveType.PAPER, count);
 			}
 		}
 	}
 	
 	@Test
 	public void testNextMoveIsScissors() {
+		System.out.printf("\n\n***Testing out for '%s' move***\n", MoveType.SCISSORS);
+		
 		boolean isLoop = true;
+		int count = 0;
 		
 		while (isLoop) {
+			count++;
 			MoveType nextMove = randomPlayer.nextMove();
+			System.out.printf("Next move is '%s'\n", nextMove.toString());
 			if (nextMove.equals(MoveType.SCISSORS)) {
 				isLoop = false;
+				System.out.printf("Got '%s' move after %d iterations\n", MoveType.SCISSORS, count);
 			}
 		}
 	}
